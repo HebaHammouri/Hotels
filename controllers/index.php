6ed7c735fa6594b5fbed8d1d176629117859ce1d
hhammouri <hhammouri@souq.com>
2018-04-25 12:53:27 +0300

    add httpful to composer

--- controllers/index.php
+++ controllers/index.php
@@ -2,14 +2,30 @@
 
 class Index extends Controller {
 
-    function __construct() {
+    public function __construct() {
         parent::__construct();
     }
     
-    function index() {
+    public function index() {
+        $URL = "https://offersvc.expedia.com/offers/v2/getOffers?scenario=deal-finder&page=foo&uid=foo&productType=Hotel";
+        $this->getHotels($URL);
         $this->view->render('header',['title'=>'Home Page']);
         $this->view->render('index/index',['default'=>'This is the main page welcome!']);
         $this->view->render('footer',[]);
+    }
+
+    private function getHotels($URL)
+    {
+        try {
+            $oResponse = \Httpful\Request::get($URL)
+                ->expectsJson()
+                ->send();
+
+           dump($oResponse);die();
+
+        } catch (Exception $e) {
+
+        }
     }
     
 }
